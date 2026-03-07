a9b791441550b317d049a986d17e65f29070022f
Eliott Wantz <eliott.wantz@develiott.com>
2026-03-07 18:44:21 -0500

    Initial Commit

---
+++ Walletify/WalletifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletifyApp.swift
+//  Walletify
+//
+//  Created by Eliott on 07-03-2026.
+//
+
+import SwiftUI
+
+@main
+struct WalletifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
